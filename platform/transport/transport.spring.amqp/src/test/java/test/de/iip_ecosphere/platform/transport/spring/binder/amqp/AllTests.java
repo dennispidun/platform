6cf2ffe7eb21ef7d28463eb4fc92fd31ae8196f4
dennispidun <37330425+dennispidun@users.noreply.github.com>
2021-10-20 21:49:22 +0200

    Revert "only TLS test, no known way to reset Spring"
    This reverts commit 4d628291ada5f99231eea2a174d06f5252946952.

--- platform/transport/transport.spring.amqp/src/test/java/test/de/iip_ecosphere/platform/transport/spring/binder/amqp/AllTests.java
+++ platform/transport/transport.spring.amqp/src/test/java/test/de/iip_ecosphere/platform/transport/spring/binder/amqp/AllTests.java
@@ -21,7 +21,7 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    //AmqpMessageBinderTest.class, // needs reset of Spring Cloud Stream for new Binder; TLS extends this test
+    AmqpMessageBinderTest.class,
     AmqpMessageBinderTlsTest.class
 })
 public class AllTests {
