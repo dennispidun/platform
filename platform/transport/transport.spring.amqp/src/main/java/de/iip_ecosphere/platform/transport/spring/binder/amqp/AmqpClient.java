ee7704676ae02109f99206a9db20d5cbaa4b7d70
dennispidun <37330425+dennispidun@users.noreply.github.com>
2021-10-20 21:49:22 +0200

    Revert "using changed BasicConfiguration"
    This reverts commit 53f8f7ed564b5dfb92810822fbbbd85ee8620711.

--- platform/transport/transport.spring.amqp/src/main/java/de/iip_ecosphere/platform/transport/spring/binder/amqp/AmqpClient.java
+++ platform/transport/transport.spring.amqp/src/main/java/de/iip_ecosphere/platform/transport/spring/binder/amqp/AmqpClient.java
@@ -121,7 +121,7 @@ public class AmqpClient {
                 if (null != config.getKeystore()) {
                     try {                
                         factory.useSslProtocol(SslUtils.createTlsContext(config.getKeystore(), 
-                            config.getKeyPassword(), config.getKeyAlias()));
+                            config.getKeystorePassword(), config.getKeyAlias()));
                     } catch (IOException e) {
                         LOGGER.error("AMQP: Loading keystore " + e.getMessage() + ". Trying with no TLS.");
                     }
